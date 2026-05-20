20ce8c1d7dca286b4c8ef70e3b9704b95184060b
fabianruiz-cmd <fabian_ruiz@cun.edu.co>
2026-05-20 14:51:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const path    = require('path');
 const app     = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || process.env.RAILWAY_PORT || 8080;
+console.log("[DEBUG] process.env.PORT =", process.env.PORT);
+console.log("[DEBUG] PORT final =", PORT);
 
 // ── Configuración ─────────────────────────────────────────────────────────────
 const ENV        = process.env.WIP_ENV || 'prod'; // 'prod' o 'qa'
